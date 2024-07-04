158399552a2c604d8038faf0b9f18d42206c18bf
Marya Kruzhilina <musatowa.marya@yandex.ru>
2024-07-04 17:53:10 +0300

    final3

--- src/main/java/ru/maryKr/bootCrud/service/UserService.java
+++ src/main/java/ru/maryKr/bootCrud/service/UserService.java
@@ -1,9 +1,7 @@
 package ru.maryKr.bootCrud.service;
 
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.transaction.annotation.Transactional;
 import ru.maryKr.bootCrud.model.User;
 
 public interface UserService {
-    public User findByUsername(String username);
+    User findByUsername(String username);
 }
